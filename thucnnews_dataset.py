1806b99bfd27f6a0e348bed880f2ebcd388cc341
leo_songwei <leo_songwei@outlook.com>
2024-08-02 11:47:15 +0800

    notes

--- thucnnews_dataset.py
+++ thucnnews_dataset.py
@@ -1,12 +1,15 @@
 import pickle
+from typing import List
 
 class ThuCnNewsSDataset:
     def __init__(self) -> None:
-        self.pickle_path = "/home/leo/NLP/datasets/THUCNNEWS/THUNews_FULL.pickle"
+        self.pickle_path = "~/THUNews_FULL.pickle"
         with open(self.pickle_path, "rb") as file:
-            self.contents = pickle.load(file)
+            self.contents: List[str] = pickle.load(file)
     
     def item_generator(self, start_file_index: int, start_item_index: int):
+        # 这个start_file_index属于我的垃圾代码，不用在意。
+        # 在早期阶段直接用的悟道等数据集的压缩文件，它是为了记录读到哪个文件了
         index = start_item_index
         
         while True:
